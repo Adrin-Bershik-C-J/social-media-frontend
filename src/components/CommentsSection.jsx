53d2911d56fcb2b475eeb4f70bd04c14ceb16846
cjabe <adrinbershik.2201008@srec.ac.in>
2025-07-08 11:07:33 +0530

    Like loader for Comment Section

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -211,8 +211,7 @@ const CommentsSection = ({ postId }) => {
                       {likeLoading[comment._id] ? (
                         // Custom red spinner SVG
                         <svg
-                          className="w-4 h-4 animate-spin text-red-600"
-                          xmlns="http://www.w3.org/2000/svg"
+                          className="animate-spin w-4 h-4 text-current"
                           fill="none"
                           viewBox="0 0 24 24"
                         >
@@ -223,12 +222,12 @@ const CommentsSection = ({ postId }) => {
                             r="10"
                             stroke="currentColor"
                             strokeWidth="4"
-                          ></circle>
+                          />
                           <path
                             className="opacity-75"
                             fill="currentColor"
-                            d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
-                          ></path>
+                            d="M4 12a8 8 0 018-8V0C5.4 0 0 5.4 0 12h4z"
+                          />
                         </svg>
                       ) : (
                         <>
