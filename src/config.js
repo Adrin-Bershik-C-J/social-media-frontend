5b783e4f4e62f29ab2c864983d8d4c7f810b3040
cjabe <adrinbershik.2201008@srec.ac.in>
2025-07-13 10:49:30 +0530

    config update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   // API_URL: "https://social-media-backend-uv33.onrender.com",
-  // API_URL: "https://social-media-backend-production-d49b.up.railway.app",
-  API_URL: "http://localhost:5000",
+  API_URL: "https://social-media-backend-production-d49b.up.railway.app",
+  // API_URL: "http://localhost:5000",
 };
 
 export default config;
